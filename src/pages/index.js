35227a17e7f1bf15bb03111ee2165ff04038580c
Reggie Peterson <reginald.peterson2@nih.gov>
2019-12-24 11:51:39 -0500

    fix typos

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,39 +2,41 @@ import React from 'react'
 
 export default () => (
   <div className="app">
-I've always been afraid of being alive. Not of waking up and existing, but of being alive, being myself. I've always felt like I've needed permission to speak or live. Always an outstretched hand, demanding payment. If I couldn't be the best, if I couldn't fix it, fix everything, then what was I doing? I wasn't good enough. It hurts to feel that way. To feel like no matter what you do, you will never be good enough. That your existence is a act of randomness and everyone else's is a stroke of intelligent design. No matter how hard you try, you can never catch your shadow, and you shadow, always hanging somewhere off to the side, can never quite catch you. It has been like that for as long as I can remember. I've been a husk of a person; A shell of a life.
+I've always been afraid of being alive. Not of waking up and existing, but of being alive, being myself. I've always felt like I've needed permission to speak or live. Always an outstretched hand, demanding payment. If I couldn't be the best, if I couldn't fix it, fix everything, then what was I doing? I wasn't good enough. It hurts to feel that way. To feel like no matter what you do, you will never be good enough. That your existence is an act of randomness and everyone else's is a stroke of intelligent design. No matter how hard you try, you can never catch your shadow, and your shadow, always hanging somewhere off to the side, can never quite catch you. It has been like that for as long as I can remember. I've been a husk of a person; A shell of a life.
 <br/><br/>
-For as long as I can recall, I've operated in survival mode. I only live to live another day and even that gets hard. Sometimes I'm tormented by the pointlessness of it all. No god to turn to, a broken family, despite love.
+For as long as I can recall, I've operated in survival mode. I only live to live another day and even that gets hard. Sometimes I'm tormented by the pointlessness of it all. No god to turn to; a broken family, despite love.
 <br/><br/>
 She's always been there for me. By my side, a force of stability and unwavering love and I don't know what to make of it. What did I do to deserve someone like this? It must not be real. It must be another trick. Just a trick of the light. Love and care and belonging are offered and it's just so foreign to me.
 <br/><br/>
 I want nothing more than to be rid of this weight on my shoulders. It feels like I'm never safe. Every minute in my skin is another dissonant chord played on the keyboard in our dining room. Two keys next to each other. Something isn't quite right. The two keys can't help they exist next to each other. They just do. I can't get in my car and drive anywhere. The enemy is part of me. He which writes this sentence is my prison and my prisoner. There is no escape from the pain and hopelessness.
 <br/><br/>
-Except there is. She offers me one. But I don't believe it's real. I never have. It's a trap that leads to more suffering. It can't be real. It's just another bar of the prison. All I can do is imagine I was better. Maybe then my dad would have stayed. Maybe then we wouldn't have suffered so much. Maybe then my mom would be happy and my grandma would have her GTO and everyone wouldn't be on some kind of drug and shit poor and unhappy and suffering. Maybe if I was better things would have been better. Maybe she wouldn't be suicidal and Leo wouldn't be anxious and not have to eat formula. Maybe the world would be happier. Maybe I wouldn't have picked the flower I found growing through the cracks of the pavement. Maybe the roots would have been stronger. Maybe the boot was a mercy; get it over quickly. Instead of constantly tugging at the roots just a little bit, but just enough, every time I walked by.
+Except there is. She offers me one. But I don't believe it's real. I never have. It's a trap that leads to more suffering. It can't be real. It's just another bar of the prison. All I can do is imagine I was better. Maybe then my dad would have stayed. Maybe then we wouldn't have suffered so much. Maybe then my mom would be happy and my grandma would have her GTO and everyone wouldn't be on some kind of drug and shit poor and unhappy and suffering. Maybe if I was better things would have been better. Maybe she wouldn't be suicidal and Leo wouldn't be anxious and cry out in his sleep. Maybe the world would be happier. Maybe I wouldn't have picked the flower I found growing through the cracks of the pavement. Maybe the roots would have been stronger. Maybe the boot was a mercy; get it over quickly. Instead of constantly tugging at the roots just a little bit, but just enough, every time I walked by.
 <br/>
 <h4>She doesn't deserve my baggage.</h4>
-I've been presented this wonderful opportunity. I don't know why but I hope to one day.
+I've been presented this wonderful opportunity; I don't know why but I hope to one day.
 <br/><br/>
 I vow to be a better listener and a gentler friend.<br/><br/>
 I vow to be the person she inspires me to be.<br/><br/>
 I vow to use my time in the service of love.<br/><br/>
 I vow to focus on the joy, when times are stressful.<br/><br/>
 I vow to do what's right for us, even when It's difficult.<br/><br/>
-I vow to let her love me; to let me love me.<br/><br/>
+I vow to let her love me, to let me love me, and to let me love her.
 <br/><br/>
-I vow that this life can be wonderfully beautiful, elegantly simple and deeply rewarding and I vow to always walk in the direction of love, even against the tide of overwhelm urges and shame and emotions.
+I vow that this life can be wonderfully beautiful, elegantly simple and deeply rewarding and I vow to always walk in the direction of love, even against the tide of overwhelming shame and fear.
 <br/><br/>
-I vow to tell her what scares me.
+I vow to tell her what I fear.
 <br/><br/>
 I vow that I'm terrified of how much I've grown used to her.
 <br/><br/>
-I vow that without her my days would be a collection of disparate manic depressive extremes.
+I vow that without her my days would be a collection of disparate manic-depressive extremes.
 <br/><br/>
 I vow that the person I am wouldn't exist without her.
 <br/><br/>
 I vow that the person I am isn't the hurt that I've hidden behind.
 <br/><br/>
-I vow that her love is such a cleansing, simple relief. It promises everything is okay and it gives me the freedom to be.
+I vow to be honest.
+<br/><br/>
+I vow that honestly, her love is such a cleansing, simple relief. It promises everything is okay and it gives me the freedom to be.
 <br/><br/>
 I vow that I will never have the right words to speak or write, but I know that she's given me the vocabulary to feel again.
 <br/><br/>
